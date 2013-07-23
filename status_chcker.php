c04e33190dc89ed98c6fe016f27d13956b23607e
Rowedahelicon <matthewr01@gmail.com>
2013-07-23 10:00:25 -0400

    Update status_chcker.php
    -Updated script to have emailing capabilities!
    -Added send_email and send_text variables to toggle sending an email or text in the event of a server offline scenario.
    -Added the my_email variable to add in your email address

--- status_chcker.php
+++ status_chcker.php
@@ -1,21 +1,25 @@
 <?php
 //YOU CAN EDIT THESE
-//THIS IS YOUR PHONENUMBER, No hyphens!
 
-$my_phone = "0000000";
+$my_email = "username@email.com"; //THIS IS YOUR EMAIL ADDRESS!
+$my_phone = "0000000000"; //THIS IS YOUR PHONENUMBER, No hyphens!
+
+//Edit these to add or remove the emailing / texting capability
+$send_email = 1;
+$send_text = 1
 
 //This is your phone service, enter in the number of the one you use from the list before
 //1 = Sprint , 2 = Verizon , 3 = T-Mobile, 4 = AT&T 
-$my_carrier = "";
+$my_carrier = 1
 
 //DON'T EDIT ANYTHING BELOW.
 
 //These are the list of available SMS gateways, you actually can add more
 //The list is available here : http://en.wikipedia.org/wiki/List_of_SMS_gateways
-$carrier[1] = "messaging.sprintpcs.com";
-$carrier[2] = "vtext.com";
-$carrier[3] = "tmomail.net";
-$carrier[4] = "txt.att.net";
+$carrier[1] = "messaging.sprintpcs.com"; //Sprint
+$carrier[2] = "vtext.com"; //Verizon
+$carrier[3] = "tmomail.net"; //T-Mobile
+$carrier[4] = "txt.att.net"; //AT&T
 
 //This is where the json variable gets used
 //change this line to 1 if you want it to always print in json
@@ -43,6 +47,7 @@ $x=0;
 $offline_servers=0;
 while(!feof($file)) {
 $ip_name = fgets($file);
+list($server_name, $ip_name) = explode("|", $ip_name, 2); 
 $ip = explode(":", $ip_name);
 $online = @fsockopen( $ip[0], $ip[1], $errno, $errstr, 200);
 $x++;
@@ -58,15 +63,26 @@ $servers = "$servers , $ip_name";
 $offline_servers++;
 }
 $ip_name = str_replace(array("\r\n", "\r", "\n"),"",$ip_name);
-if($getJson){ echo"{ \"ip\":\"$ip_name\" , \"status\":\"$status_clean\" },\n"; }else{ echo "$ip_name : $status<br/>"; }
+if($getJson){ echo"{ \"ip\":\"$ip_name\" , \"server\":\"$server_name\" , \"status\":\"$status_clean\" },\n"; }else{ echo "$server_name : $ip_name : $status<br/>"; }
 }
 //Json end catch, badly written perhaps, but it has to be there! Also it doesn't affect any json queries since it doesn't match the other outputs.
 if($getJson){ echo"{ \"completed\":\"yes\" }\n]\n} "; }
 
+
 if($offline_servers>=1){
-mail("$my_phone@$carrier[$my_carrier]", "ALERT THE FOLLOWING ARE OFFLINE", "$servers");
+
+//If send_email is set to 1, this line sends the email!
+if($send_email>=1){
+$to = $my_email;
+$subject = "Server status report!";
+$message = "Hello! I'm sorry to inform you that the following are offline! $servers";
 }
 
+//If send_text is set to 1, this line sends the text!
+if($send_text>=1){
+mail("$my_phone@$carrier[$my_carrier]", "ALERT THE FOLLOWING ARE OFFLINE", "$servers");
+}
+}
 if($print_debug){ echo" Phone : $my_phone , Carrier : $my_phone@$carrier[$my_carrier] , Offline : $offline_servers"; }
 
 fclose($file);
